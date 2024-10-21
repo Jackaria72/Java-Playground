6bcc48a13be1a871e6cf2d3f0909da8937d6c043
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-10-21 20:26:48 +0100

    Feat: Implement StringEndsWithKata Methodology

--- src/main/java/CodeWars/Katas.java
+++ src/main/java/CodeWars/Katas.java
@@ -1,4 +1,12 @@
 package CodeWars;
 
 public class Katas {
+
+
+     /* STRING ENDS WITH
+     Complete the solution so that it returns true if the first argument(string)
+     passed in ends with the 2nd argument (also a string). */
+     public static boolean SEWSolution(String str, String ending) {
+         return str.endsWith(ending);
+     }
 }
