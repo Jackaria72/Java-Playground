8dad8141b5ff1172e2f16382a2076215a709af14
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-10-24 21:07:12 +0100

    Feat: create method for Vowel Count Kata

--- src/main/java/CodeWars/Katas.java
+++ src/main/java/CodeWars/Katas.java
@@ -16,4 +16,12 @@ public class Katas {
     public static String RSSolution(String str) {
         return new StringBuilder(str).reverse().toString();
     }
+    /* VOWEL COUNT - https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/java
+        Return the number (count) of vowels in the given string.
+        We will consider a, e, i, o, u as vowels for this Kata (but not y).
+        The input string will only consist of lower case letters and/or spaces.
+     */
+    public static int VCSolution(String str) {
+        return 0;
+    }
 }
