51b712518ec63689f86cc8855605f46c21d9f6e8
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-10-27 22:30:50 +0000

    Refactor: change method names to be more descriptive

--- src/main/java/CodeWars/Katas.java
+++ src/main/java/CodeWars/Katas.java
@@ -1,21 +1,19 @@
 package CodeWars;
 
-import java.util.ArrayList;
-
 public class Katas {
 
 
      /* STRING ENDS WITH - https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
      Complete the solution so that it returns true if the first argument(string)
      passed in ends with the 2nd argument (also a string). */
-     public static boolean SEWSolution(String str, String ending) {
+     public static boolean stringEndsWithKataSolution(String str, String ending) {
          return str.endsWith(ending);
      }
 
      /* REVERSED STRINGS - https://www.codewars.com/kata/5168bb5dfe9a00b126000018
      Complete the solution so that it reverses the string passed into it.
       */
-    public static String RSSolution(String str) {
+    public static String reversedStringsKataSolution(String str) {
         return new StringBuilder(str).reverse().toString();
     }
     /* VOWEL COUNT - https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/java
@@ -23,7 +21,7 @@ public class Katas {
         We will consider a, e, i, o, u as vowels for this Kata (but not y).
         The input string will only consist of lower case letters and/or spaces.
      */
-    public static int VCSolution(String str) {
+    public static int vowelCountKataSolution(String str) {
         int vowelCount = 0;
         for (int i = 0; i < str.length(); i++) {
             if (str.charAt(i) == 'a' || str.charAt(i) == 'e' ||
