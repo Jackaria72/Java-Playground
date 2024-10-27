ca11281c2529606c5617ff20fdf52830098ab2e8
Jackaria72 <jack.goodwill@hotmail.co.uk>
2024-10-27 22:34:25 +0000

    Refactor: add kyu level to description comment

--- src/main/java/CodeWars/Katas.java
+++ src/main/java/CodeWars/Katas.java
@@ -4,6 +4,7 @@ public class Katas {
 
 
      /* STRING ENDS WITH - https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+     <7 Kyu>
      Complete the solution so that it returns true if the first argument(string)
      passed in ends with the 2nd argument (also a string). */
      public static boolean stringEndsWithKataSolution(String str, String ending) {
@@ -11,12 +12,14 @@ public class Katas {
      }
 
      /* REVERSED STRINGS - https://www.codewars.com/kata/5168bb5dfe9a00b126000018
+     <8 Kyu>
      Complete the solution so that it reverses the string passed into it.
       */
     public static String reversedStringsKataSolution(String str) {
         return new StringBuilder(str).reverse().toString();
     }
-    /* VOWEL COUNT - https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/java
+    /* VOWEL COUNT - https://www.codewars.com/kata/54ff3102c1bad923760001f3
+    <7 Kyu>
         Return the number (count) of vowels in the given string.
         We will consider a, e, i, o, u as vowels for this Kata (but not y).
         The input string will only consist of lower case letters and/or spaces.
@@ -32,7 +35,8 @@ public class Katas {
 
         return vowelCount;
     }
-    /* Count by X - https://www.codewars.com/kata/5513795bd3fafb56c200049e/train/java
+    /* Count by X - https://www.codewars.com/kata/5513795bd3fafb56c200049e
+    <8 Kyu>
         Create a function with two arguments that will return an array of the first n multiples of x.
         Assume both the given number and the number of times to count will be positive numbers greater than 0.
         Return the results as an array or list ( depending on language ).
